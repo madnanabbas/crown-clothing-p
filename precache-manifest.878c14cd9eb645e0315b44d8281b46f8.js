5c51de13ce6341854e527000d891b1625588295d
Adnan Abbas <madnanabbas14@gmail.com>
2020-05-13 13:22:29 +0500

    Updates

---
+++ precache-manifest.878c14cd9eb645e0315b44d8281b46f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a789fce054d54c1aa98554c985394b7",
+    "url": "/crown-clothing-p/index.html"
+  },
+  {
+    "revision": "26533cc7d8ef6463b700",
+    "url": "/crown-clothing-p/static/css/main.be8c3796.chunk.css"
+  },
+  {
+    "revision": "ef7d9dcdbf407d411ace",
+    "url": "/crown-clothing-p/static/js/2.88bcc801.chunk.js"
+  },
+  {
+    "revision": "f9b3e87900b5f3843f2b97c9b5b24d1e",
+    "url": "/crown-clothing-p/static/js/2.88bcc801.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26533cc7d8ef6463b700",
+    "url": "/crown-clothing-p/static/js/main.abb5eb98.chunk.js"
+  },
+  {
+    "revision": "0faffecaf7b9781bac70",
+    "url": "/crown-clothing-p/static/js/runtime-main.862940bd.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crown-clothing-p/static/media/crown.ed28accb.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/crown-clothing-p/precache-manifest.878c14cd9eb645e0315b44d8281b46f8.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/crown-clothing-p/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
